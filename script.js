b4080dadbc4323bc30ad92e4addb90c96d9c1b5c
Adebisi Favour <adebisimomoluwa@gmail.com>
2025-02-12 12:50:24 +0100

    script

---
+++ script.js
@@ -0,0 +1,50 @@
+alert(987)
+console.log(987)
+
+alert('Femi')
+console.log("Idowu24'./.;'...");
+
+// // STRINGS
+"Femi", "1234", "Ranti12345"
+
+// // Number
+123, 35, -9, 0.65, .689, 76.65
+
+// // Boolean
+true, false
+
+// // Array
+[1, "Hello", true]
+
+// // Object
+{Firstname:"Tolu", 'Lastname';"Iyaniwura";}
+console.log(false);
+
+console.log(typeof(""));
+
+var studentMatricNumber = "Matthew 112345"
+console.log(studentMatricNumber);
+
+console.log(245==245);
+console.log(245==="245")
+
+var x =20
+var y = 30
+ 
+console.log("The result is " +x+y);
+console.log(2**3);
+console.log(7%4);
+
+var firstnumber =Number(prompt("Enter the first number")) 
+var secondNumber =Number(prompt("Enter the second number"))
+var answer =(firstnumber + secondNumber)   //solve the bug
+console.log(answer);
+
+
+ var thatnum = prompt("enter the first value")
+ console.log(thatnum ** 2);
+
+
+var x = 5;
+var y = 6;
+var z = x + y;
